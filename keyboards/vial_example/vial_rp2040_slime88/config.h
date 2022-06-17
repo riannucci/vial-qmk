4a16022872769721b67b2584d137f50edeb08188
zykrah <zykrahgaming@gmail.com>
2022-06-18 05:02:30 +1000

    Temp fix: ADC disabled

--- keyboards/vial_example/vial_rp2040_slime88/config.h
+++ keyboards/vial_example/vial_rp2040_slime88/config.h
@@ -3,20 +3,19 @@
 #pragma once
 
 /* USB Device descriptor parameter */
-#define VENDOR_ID       0x7A79 /* "ZY" */
-#define PRODUCT_ID      0x0088
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x0000
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    Zykrah
-#define PRODUCT         Slime88
+#define MANUFACTURER    Vial
+#define PRODUCT         RP2040 example
 
 /* key matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 17
+#define MATRIX_ROWS 2
+#define MATRIX_COLS 2
 
-#define MATRIX_ROW_PINS { 2, 29, 28, 27, 11, 12 }
-#define MATRIX_COL_PINS { 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 0, 1, 3 }
+#define MATRIX_ROW_PINS { 26, 0 }
+#define MATRIX_COL_PINS { 1,  3 }
 
-/* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
